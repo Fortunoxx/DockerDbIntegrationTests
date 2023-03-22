c55f575d1898c78a9e76fe6024dcb02291b10fe5
Rob Gates <robert_goetz@hotmail.com>
2023-03-22 20:47:49 +0100

    added use case id = 0

--- test/SomeWebApiIntegrationTests/ProcessController/ProcessActivityTests.cs
+++ test/SomeWebApiIntegrationTests/ProcessController/ProcessActivityTests.cs
@@ -46,6 +46,37 @@ public sealed class ProcessActivityTests
     }
 
     [Fact]
+    internal async Task Process_Should_Fail_Invalid_Id_Async()
+    {
+        await using var provider = new ServiceCollection()
+          .AddMassTransitTestHarness(x =>
+          {
+              x.SetKebabCaseEndpointNameFormatter();
+              x.AddActivity<ProcessActivity, ProcessActivityArguments, ProcessActivityLog>();
+          })
+          .BuildServiceProvider(true);
+
+        var harness = provider.GetTestHarness();
+
+        await harness.Start();
+
+        var builder = new RoutingSlipBuilder(Guid.NewGuid());
+        builder.AddSubscription(harness.Bus.Address, RoutingSlipEvents.All);
+
+        var fixture = new Fixture();
+
+        builder.AddActivity("Activity to be tested",
+            new Uri("loopback://localhost/process_execute"),
+            fixture.Build<ProcessActivityArguments>().With(x => x.Id, 0).Create());
+
+        var routingSlip = builder.Build();
+        await harness.Bus.Execute(routingSlip);
+
+        (await harness.Sent.Any<RoutingSlipActivityCompleted>()).Should().BeFalse("first activity should fail");
+        (await harness.Sent.Any<RoutingSlipFaulted>()).Should().BeTrue("activity failed, so the routing slip failed");
+    }
+
+    [Fact]
     internal async Task Process_With_Compensation_Should_Work_Async()
     {
         await using var provider = new ServiceCollection()
