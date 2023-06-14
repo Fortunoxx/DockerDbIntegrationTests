c02bb56b37fff06f306d486fbead6facbd986676
Fortunoxx <robert_goetz@hotmail.com>
2023-06-14 23:23:05 +0200

    fine tuning

--- test/SomeWebApiIntegrationTests/FakeData/UserFaker.cs
+++ test/SomeWebApiIntegrationTests/FakeData/UserFaker.cs
@@ -1,6 +1,7 @@
 namespace SomeWebApiIntegrationTests.FakeData;
 
 using AutoBogus;
+using Bogus.Extensions;
 using SomeWebApi.Model;
 
 public class UserFaker : AutoFaker<User>
@@ -13,7 +14,7 @@ public class UserFaker : AutoFaker<User>
     {
         RuleFor(fake => fake.AboutMe, x => x.Name.JobDescriptor());
         RuleFor(fake => fake.DisplayName, x => x.Name.FullName());
-        RuleFor(fake => fake.EmailHash, x => x.Internet.Email(provider: domain));
+        RuleFor(fake => fake.EmailHash, x => x.Internet.Email(provider: domain).ClampLength(1,40));
         RuleFor(fake => fake.Location, x => x.Address.City());
         RuleFor(fake => fake.WebsiteUrl, x => x.Internet.Url());
     }
